4928a9de4ffc1a0579e716d04551d53101800333
aberry5621 <aberry5621@gmail.com>
2017-04-09 13:17:44 -0700

    I FInished

--- LABEXy2017m04d06/main.cpp
+++ LABEXy2017m04d06/main.cpp
@@ -38,7 +38,7 @@ public:
     LivingThing() {}
     LivingThing(int p) { legs = p; }
     int legs;
-    virtual void move() { cout << "Some living thing is moving!\n"; };
+    virtual void move() { cout << "Some living thing is moving "; };
 };
 
 class Monkey : public LivingThing
@@ -48,7 +48,7 @@ public:
     //	constructor using the initializer 2 for number of legs
     Monkey() : LivingThing(2) {}
     //TO DO: Override the 'move' function to display "Monkey is scampering!"
-    
+    void move() override { cout << "A monkey is scampering "; };
 };
 
 class Human : public LivingThing
@@ -57,9 +57,8 @@ public:
     //TO DO: declare a no-arg constructor, it should call the base class
     //	constructor using the initializer 2 for number of legs
     Human() : LivingThing(2) {}
-    
     //TO DO: Override the 'move' function to display "Human is walking!"
-    
+    void move() override { cout << "A human is walking "; };
 };
 
 class Tiger : public LivingThing
@@ -68,9 +67,8 @@ public:
     //TO DO: declare a no-arg constructor, it should call the base class
     //	constructor using the initializer 4 for number of legs
     Tiger() : LivingThing(4) {}
-    
     //TO DO: Override the 'move' function to display "Tiger is prowling!"
-    
+    void move() override { cout << "A tiger is prowling "; };
 };
 
 class Centipede : public LivingThing
@@ -79,21 +77,19 @@ public:
     //TO DO: declare a no-arg constructor, it should call the base class
     //	constructor using the initializer 4 for number of legs
     Centipede() : LivingThing(100) {}
-    
     //TO DO: Override the 'move' function to display "The centipede is crawling!"
-    
+    void move() override { cout << "The centipede is crawling "; };
 };
 
 int main() {
-    // insert code here...
     std::cout << "Living Things!\n";
     
-    srand(static_cast<int>(time(0))); // randome seed
+    int const ARR_SIZE = 4;
     
     cout << "Part I. Static Bindings\n";
     cout << "=======================\n";
     //Declare an array of LivingThing base type, of size 4
-    int const ARR_SIZE = 4;
+    
     
     LivingThing creatures [ARR_SIZE];
     //Randomly populate the array with a mix of humans and tigers,
@@ -106,20 +102,37 @@ int main() {
     
     //Use a loop, call the "move" virtual function from the base class
     for (int i = 0; i < ARR_SIZE; i++) {
-        cout << "Creature number "
-        << i + 1 << " has "
-        << creatures[i].legs << " legs!" << endl;
+        creatures[i].move();
+        cout << "on " << creatures[i].legs << " legs.\n";
     }
     
     cout << "Part II. Dynamic Bindings\n";
     cout << "=========================\n";
     //Declare an array of pointers of LivingThing base type, of size 4
     
+    LivingThing * dynamic_creatures [ARR_SIZE];
+    Monkey monkey = Monkey();
+    Tiger tiger = Tiger();
+    Human human = Human();
+    Centipede centipede = Centipede();
+    LivingThing * tmpPtr;
+    tmpPtr = &monkey;
+    dynamic_creatures[0] = tmpPtr;
+    tmpPtr = &tiger;
+    dynamic_creatures[1] = tmpPtr;
+    tmpPtr = &human;
+    dynamic_creatures[2] = tmpPtr;
+    tmpPtr = &centipede;
+    dynamic_creatures[3] = tmpPtr;
+    
     //Randomly populate the array with a mix of pointers to humans and tigers
     
     //Use a loop, call the "move" function with each LivingThing object in the
-    
     //	array
+    for (int i = 0; i < ARR_SIZE; i++) {
+        dynamic_creatures[i]->move();
+        cout << "on " << dynamic_creatures[i]->legs << " legs.\n";
+    }
     
     return 0;
 }
