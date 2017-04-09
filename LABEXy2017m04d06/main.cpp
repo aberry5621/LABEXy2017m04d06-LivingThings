6195c81a455df8a61251d2415ea1affbd7dbb1bf
aberry5621 <aberry5621@gmail.com>
2017-04-09 09:52:27 -0700

    Initial Commit

---
+++ LABEXy2017m04d06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LABEXy2017m04d06
+//
+//  Created by ax on 4/9/17.
+//  Copyright © 2017 COMP235. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
